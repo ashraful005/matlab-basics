ab4486fd7689dc7f246131a6404b1046e931b2e4
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-04-02 14:26:27 +0600

    matlab function

---
+++ functions/sum.m
@@ -0,0 +1,4 @@
+a = 3;
+b = 4;
+c = sum_func(a, b);
+disp(c); % Output: 7
---
+++ functions/sum_func.m
@@ -0,0 +1,6 @@
+function z = sum_func(x, y)
+% This function takes two input arguments (x and y) and returns their sum (z)
+
+z = x + y;
+
+end
