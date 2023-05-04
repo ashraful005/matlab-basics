028e5ed265c823c744434a7d10c48687ecda7eaa
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-05-04 02:21:47 -0700

    matrix dot() function

--- matrix_arithmatic.m
+++ matrix_arithmatic.m
@@ -7,9 +7,13 @@ C=[9 10; 11 12];
 A+B
 A-B
 A*B
-B2=[1 2; 3 4; 5 6]
+B2=[1 2; 3 4; 5 6];
 %A*B2    %GIVES ERROR DUE TO DIMENSION RULE MISMATCH
 A*B2'    %NO ERROR
 A/B
 A.*B %dot product(element by element)
 A./B  %element by element division
+
+x=[1 2 3];
+y=[4 5 6];
+dot(x,y)
