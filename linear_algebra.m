49040c3fa48a71816ff563c099234a2657d9464d
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-05-04 03:49:58 -0700

    euclidian norm

--- linear_algebra.m
+++ linear_algebra.m
@@ -8,4 +8,6 @@ A\b';
 det(A)  %determinant
 eig(A)   %eigenvalues
 [v,D]= eig(A)  %eigenvalues and eigenvectors
-A*v - v*D
+A*v - v*D  %A*v == v*D should be true
+norm(A*v-v*D)  %euclidian norm
+norm(A*v-v*D, 2)
