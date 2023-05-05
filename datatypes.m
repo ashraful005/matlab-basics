919a4b44878850276e8377cf110fd79b458f3889
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-05-04 22:31:30 -0700

    structure extended

--- datatypes.m
+++ datatypes.m
@@ -2,3 +2,9 @@ clc;
 clear all;
 close all;
 my_struct.name='Ashraful'
+my_struct.age=23
+my_struct
+isfield(my_struct, 'name')
+rmfield(my_struct, 'age')
+my_struct
+setfield(my_struct, 'gender', 'f')
